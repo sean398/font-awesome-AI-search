f3ca5337c6ddee506a3456a27735e8dbc6b04b4e
Sean398 <guohaochuan5461@gmail.com>
2021-05-28 16:45:03 +0800

    update UI

--- src/components/icon-display/icon-gallery.component.jsx
+++ src/components/icon-display/icon-gallery.component.jsx
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { ICONMAP } from "../../constant/icon-map";
+import { Input } from "antd";
 import "./icon-gallery.style.scss";
 
 const IconGallery = () => {
+  const [iconList, setIconList] = useState(Object.keys(ICONMAP));
+  const handleSearch = (value) => {
+    if (!value) setIconList(Object.keys(ICONMAP));
+    const filteredLIst = Object.keys(ICONMAP).filter((name) => {
+      return name.includes(value.toLocaleLowerCase());
+    });
+    setIconList(filteredLIst);
+  };
+  const HandleInputChange = (e) => {
+    const value = e.target.value;
+    if (!value) setIconList(Object.keys(ICONMAP));
+    const filteredLIst = Object.keys(ICONMAP).filter((name) => {
+      return name.includes(value.toLocaleLowerCase());
+    });
+    setIconList(filteredLIst);
+  };
   return (
-    <ul className="icon-gallery-wrapper">
-      {Object.keys(ICONMAP).map((name) => {
-        return (
-          <li key={name}>
-            <div className="icon-wrapper">
-              <i className={`fa ${name}`}></i>{" "}
-            </div>
-            <div className="desc">{name}</div>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <div className="icon-gallery-seach">
+        <Input.Search
+          placeholder="input search value"
+          onSearch={handleSearch}
+          onChange={HandleInputChange}
+        />
+      </div>
+      <ul className="icon-gallery-wrapper">
+        {iconList.map((name) => {
+          return (
+            <li key={name}>
+              <div className="icon-wrapper">
+                <i className={`fa ${name}`}></i>{" "}
+              </div>
+              <div className="desc">{name}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 
