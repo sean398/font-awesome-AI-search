2b0ebb25b51c537bc2a31b5215aa3f7ae795bd0b
Sean398 <guohaochuan5461@gmail.com>
2021-05-29 23:49:37 +0800

    import drap from antd

--- src/components/icon-display/icon-gallery.component.jsx
+++ src/components/icon-display/icon-gallery.component.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import { ICONMAP } from "../../constant/icon-map";
 import { Input } from "antd";
+import { CameraOutlined } from "@ant-design/icons";
+import { Upload, message } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
 import "./icon-gallery.style.scss";
+
+const { Dragger } = Upload;
 
 const IconGallery = () => {
   const [iconList, setIconList] = useState(Object.keys(ICONMAP));
@@ -20,9 +25,40 @@ const IconGallery = () => {
     });
     setIconList(filteredLIst);
   };
+
+  const onReaderLoad = (readerEvt) => {
+    // setImgUrl(readerEvt.target.result);
+    const img = new Image();
+    // img.onload = onImgLoad;
+    img.setAttribute("crossOrigin", "anonymous");
+    img.src = readerEvt.target.result;
+  };
+
+  const onFileChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = onReaderLoad;
+    reader.readAsDataURL(file);
+  };
+
+  const handleUpdateImg = () => {};
+
   return (
     <>
       <div className="icon-gallery-seach">
+        <CameraOutlined onClick={handleUpdateImg} />
+        <Dragger>
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">
+            Click or drag file to this area to upload
+          </p>
+          <p className="ant-upload-hint">
+            Support for a single or bulk upload. Strictly prohibit from
+            uploading company data or other band files
+          </p>
+        </Dragger>
         <Input.Search
           placeholder="input search value"
           onSearch={handleSearch}
